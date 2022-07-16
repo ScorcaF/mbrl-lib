768196c85bd2a01f745e3deb42754eb179cdf07b
francesco scorca <70110839+ScorcaF@users.noreply.github.com>
2022-07-16 13:17:24 +0200

    Update reward_fns.py
    added car1D toy reward function

--- mbrl/env/reward_fns.py
+++ mbrl/env/reward_fns.py
@@ -51,3 +51,9 @@ def pusher(act: torch.Tensor, next_obs: torch.Tensor) -> torch.Tensor:
     act_cost = 0.1 * (act**2).sum(axis=1)
 
     return -(obs_cost + act_cost).view(-1, 1)
+
+#################à
+
+def car1D(act: torch.Tensor, next_obs: torch.Tensor) -> torch.Tensor:
+    assert len(next_obs.shape) == len(act.shape) == 2
+    return torch.Tensor([[-0.1]]*10_000).to(next_obs.device)
